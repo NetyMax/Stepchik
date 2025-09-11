01d50defaa2996363af266131507d629abe5ae37
NetyMax <dl9akkov@gmail.com>
2025-09-11 22:20:18 +0300

    New file

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var t int = 93
+	o := 100
+	i := o - t
+	fmt.Println(i)
+
+	var a int = 1
+	fmt.Scan(&a)
+	b := 2
+	c := (a * b) + 100
+	fmt.Println(c)
+	222
+}
